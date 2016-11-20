2034bb63812c525421ea7159e18101899c30896a
Jason Lee <jleeisme@gmail.com>
2016-11-20 14:37:23 -0800

    Added the jquery cdn instead
    Slide is now working for the images too

--- js/main.js
+++ js/main.js
@@ -1,31 +1,51 @@
 (function($) {
+  var $pswp = $('.pswp')[0];
+  var image = [];
 
-  var pswpElement = document.querySelectorAll('.pswp')[0];
+  $('.image-holder-inner').each( function() {
+    var $pic = $(this),
+      getItems = function() {
+        var items = [];
+        console.log(items);
+        $pic.find('a').each(function() {
+          var $href = $(this).attr('href'),
+            $size = $(this).data('size').split('x'),
+            $width = $size[0],
+            $height = $size[1];
 
-  // build items array
-  var items = [];
-    // {
-    //   src: 'https://placekitten.com/600/400',
-    //   w: 600,
-    //   h: 400
-    // },
-    // {
-    //   src: 'https://placekitten.com/1200/900',
-    //   w: 1200,
-    //   h: 900
-    // }
+          var item = {
+            src : $href,
+            w : $width,
+            h : $height
+          }
+
+          items.push(item);
+        });
+        return items;
+      }
+      var items = getItems();
+
+      $.each(items, function(index, value) {
+        image[index] = new Image();
+        image[index].src = value['src'];
+      });
+
+      $pic.on('click', 'figure', function(event) {
+        event.preventDefault();
+        
+        var $index = $(this).index();
+        var options = {
+          index: $index,
+          bgOpacity: 0.7,
+          showHideOpacity: true
+        }
+
+        var lightBox = new PhotoSwipe($pswp, PhotoSwipeUI_Default, items, options);
+        lightBox.init();
+      });
+  });
+})(jQuery);
 
 
-  // define options (if needed)
-  var options = {
-    // optionName: 'option value'
-    // for example:
-    index: 0 // start at first slide
-  };
 
-  // move to an on click event on a picture
-  // Initializes and opens PhotoSwipe
-  // var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
-  // gallery.init();
 
-});
