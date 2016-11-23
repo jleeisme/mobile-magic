4dda52d75ee573886d268b45c8e647d45825635c
Jason Lee <jleeisme@gmail.com>
2016-11-23 13:59:39 -0800

    Added scotchPanels
    Also added a burger that animates when clicked. Once clicked the side panel pulls out. A lot of css added for the icon whereas ScotchPanels was fairly tidy. Got too caught up, forgot to commit earlier

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 (function($) {
   
+  // header scroller
   $(window).scroll(function () {
     var scroll = $(window).scrollTop()
     if (scroll > 100) {
@@ -9,15 +10,31 @@
     }
   });
 
+  // transition for the nav-icon to open and close
+  $('#nav-icon').on('click', function(){
+    $(this).toggleClass('open');
+  });
 
+  // off canvas panel
+  var panelExample = $('#panel').scotchPanel({
+    containerSelector: 'body', // Make this appear on the entire screen
+    direction: 'right', // Make it toggle in from the left
+    duration: 300, // Speed in ms how fast you want it to be
+    transition: 'ease', // CSS3 transition type: linear, ease, ease-in, ease-out, ease-in-out, cubic-bezier(P1x,P1y,P2x,P2y)
+    clickSelector: '#nav-icon', // Enables toggling when clicking elements of this class
+    distanceX: '30%', // Size fo the toggle
+    enableEscapeKey: true // Clicking Esc will close the panel
+  });
+
+  // Photoswipe
   var $pswp = $('.pswp')[0];// necessary for Photoswipe
   var image = [];
 
+  // Grabs all the images and places them into the items array
   $('.image-holder-inner').each( function() {
     var $pic = $(this),
       getItems = function() {
         var items = [];
-        // console.log(items);
         $pic.find('a').each(function() {
           var $href = $(this).attr('href'),
             $size = $(this).data('size').split('x'),
@@ -58,6 +75,13 @@
       });
   });
 })(jQuery);
+
+// $(".mobile_nav").click(function() {
+//   $(".mobile_nav_menu").animate({right: 0});
+// })
+// $(".mobile_close").click(function() {
+//   $(".mobile_nav_menu").animate({right: -270});
+// })
 
 
 
